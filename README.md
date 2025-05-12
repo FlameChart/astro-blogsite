ae56f6c151781f88dcde8c56111aa0ce3337e2d9
arc/welki/FlameC <91359913+FlameChart@users.noreply.github.com>
2025-05-12 22:32:42 +0800

    refactor: styling with Prettier.
    `prettier . -w --tab-width 4`

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # astro-blogsite
+
 Some random things I've written. now powered with Astro and GHPages.
 
 ## Quick Action
 
 Run the Astro server:
+
 ```bash
 pnpm run dev
 ```
 
 Run the styling tools `Prettier` and write changes:
+
 ```bash
 pnpm exec prettier . -w
 ```
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import {defineConfig} from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
@@ -10,12 +10,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        plugins: [tailwindcss()]
+        plugins: [tailwindcss()],
     },
 
     integrations: [mdx(), sitemap(), react()],
     i18n: {
         locales: ["en", "zh-CN"],
         defaultLocale: "en",
-    }
+    },
 });

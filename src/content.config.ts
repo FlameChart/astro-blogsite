2c6c2be76689abcf3a7d69a7b739dbaa8e1ae38b
arc/welki/FlameC <91359913+FlameChart@users.noreply.github.com>
2025-07-27 22:09:49 +0800

    feat: add new post and make the basic post page.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,13 @@
-import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/blog" }),
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        pubDate: z.coerce.date(),
-    }),
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/post" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+  }),
 });
 
 export const collections = { blog };

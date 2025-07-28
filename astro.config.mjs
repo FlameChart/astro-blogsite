082d8ec9fd0b29a26b6f5cd2b9b422610889341c
arc/welki/FlameC <91359913+FlameChart@users.noreply.github.com>
2025-07-28 13:53:34 +0800

    docs: update blogpost.

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,7 +35,8 @@ export default defineConfig({
         name: "Fira Code",
         cssVariable: "--google-font-fira-code"
       }
-    ]
+    ],
+    headingIdCompat: true,
   },
 
   prefetch: true,
